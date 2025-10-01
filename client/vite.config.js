71b6f9f53eb2ea182bd6ff25d57690325b803724
AnhTuan1678 <21020397@vnu.edu.vn>
2025-10-02 06:01:11 +0700

    2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-// https://vite.dev/config/
+// Tạo __dirname cho ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [
     react({
@@ -10,4 +15,8 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    outDir: resolve(__dirname, '../server/dist'), // build trực tiếp sang server/dist
+    emptyOutDir: true,
+  },
 })
